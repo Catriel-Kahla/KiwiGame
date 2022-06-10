00fee01682fa9fb97615354f97cf9d66a79c3741
Catriel Gabriel Kahla <94947330+Catriel-Kahla@users.noreply.github.com>
2022-06-10 02:07:42 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@
 
 let breadStock = 0;
 let appleStock = 0;
+let carrotStock = 0;
+let pumpkinStock = 0;
+let pumpkinPieStock = 0;
 let money = 0;
 
 let ready = false;
 let appleCustomer = 0;
 let breadCustomer = 0;
+let carrotCustomer = 0;
+let pumpkinPieCustomer = 0;
 
 let fullAppleStock = 20;
 let fullBreadStock = 20;
@@ -24,6 +29,8 @@ sound3.src = "sounds/pacman-come-cereza_1635131044.mp3";
 
 let breadAwards = [false, false];
 let appleAwards = [false, false];
+let carrotAwards = [false, false];
+let pumpkinPieAwards = [false, false];
 
 //foods
 
@@ -34,6 +41,7 @@ let foods = [
         stockLimit: 20,
         offer: 0,
         increase: 0,
+        sold: 0,
     },
     {
         name: "Apple",
@@ -41,8 +49,36 @@ let foods = [
         stockLimit: 20,
         offer: 0,
         increase: 0,
+        sold: 0,
+    },{
+        name: "Carrot",
+        price: 18,
+        stockLimit: 20,
+        offer: 0,
+        increase: 0,
+        lock: true,
+        sold: 0,
+    },
+    {
+        name: "Pumpkin",
+        price: 20,
+        stockLimit: 10,
+        offer: 0,
+        increase: 0,
+        lock: true,
+    },
+    {
+        name: "PumpkinPie",
+        price: 25,
+        stockLimit: 15,
+        offer: 0,
+        increase: 0,
+        lock: true,
+        sold: 0,
     }
 ]
+
+let pumpkinPieLimit = false;
 
 //functions
 
@@ -50,15 +86,27 @@ function sell(){
     readyToSell();
     if(ready == false){
         sound2.play();
-        alert("Keep working");
+        swal("Prepara más comida!", {
+            icon: "warning",
+        });
     }else{
         money = money + breadCustomer*(foods[0].price+foods[0].increase-foods[0].offer);
         money = money + appleCustomer*(foods[1].price+foods[1].increase-foods[1].offer);
+        money = money + carrotCustomer*(foods[2].price+foods[2].increase-foods[2].offer);
+        money = money + pumpkinPieCustomer*(foods[4].price+foods[4].increase-foods[4].offer);
         breadStock = breadStock - breadCustomer;
         appleStock = appleStock - appleCustomer;
+        carrotStock = carrotStock - carrotCustomer;
+        pumpkinPieStock = pumpkinPieStock - pumpkinPieCustomer;
+        foods[0].sold = foods[0].sold + breadCustomer;
+        foods[1].sold = foods[1].sold + appleCustomer;
+        foods[2].sold = foods[2].sold + carrotCustomer;
+        foods[4].sold = foods[4].sold + pumpkinPieCustomer;
 
         document.getElementById("breadStock").innerHTML = "x" + breadStock;
         document.getElementById("appleStock").innerHTML = "x" + appleStock;
+        document.getElementById("carrotStock").innerHTML = "x" + carrotStock;
+        document.getElementById("pumpkinPieStock").innerHTML = "x" + pumpkinPieStock;
         document.getElementById("cash").innerHTML = "$" + money;
 
         ask();
@@ -76,6 +124,30 @@ function makeApple(){
     appleStock = appleStock + 1;
     document.getElementById("appleStock").innerHTML = "x" + appleStock;
     foodLeft();
+}
+
+function makeCarrot(){
+    carrotStock = carrotStock + 1;
+    document.getElementById("carrotStock").innerHTML = "x" + carrotStock;
+    foodLeft();
+}
+
+function makePumpkin(){
+    pumpkinStock = pumpkinStock + 1;
+    document.getElementById("pumpkinStock").innerHTML = "x" + pumpkinStock;
+    foodLeft();
+}
+
+function makePumpkinPie(){
+    if(pumpkinStock>=2){
+        pumpkinStock = pumpkinStock - 2;
+        pumpkinPieStock = pumpkinPieStock + 1;
+        document.getElementById("pumpkinPieStock").innerHTML = "x" + pumpkinPieStock;
+        document.getElementById("pumpkinStock").innerHTML = "x" + pumpkinStock;
+        foodLeft();
+    }else if(pumpkinPieLimit==true){
+        
+    }else{alert("Necesitas más calabazas");}
 }
 
 function foodLeft(){
@@ -98,6 +170,39 @@ function foodLeft(){
     }else{
         document.getElementById("breadSignD").style.backgroundColor = "rgba(215, 245, 250, 0.596)";
     }
+
+    if(carrotStock==foods[2].stockLimit){
+        document.getElementById("carrotSignD").style.backgroundColor = "rgba(255, 190, 190, 0.808)";
+    }else if(carrotStock==foods[2].stockLimit+1){
+        document.getElementById("carrotSignD").style.backgroundColor = "rgba(255, 190, 190, 0.808)";
+        carrotStock = carrotStock - 1;
+        document.getElementById("carrotStock").innerHTML = "x" + carrotStock;
+    }else{
+        document.getElementById("carrotSignD").style.backgroundColor = "rgba(215, 245, 250, 0.596)";
+    }
+
+    if(pumpkinStock==foods[3].stockLimit){
+        document.getElementById("pumpkinSignD").style.backgroundColor = "rgba(255, 190, 190, 0.808)";
+    }else if(pumpkinStock==foods[3].stockLimit+1){
+        document.getElementById("pumpkinSignD").style.backgroundColor = "rgba(255, 190, 190, 0.808)";
+        pumpkinStock = pumpkinStock - 1;
+        document.getElementById("pumpkinStock").innerHTML = "x" + pumpkinStock;
+    }else{
+        document.getElementById("pumpkinSignD").style.backgroundColor = "rgba(215, 245, 250, 0.596)";
+    }
+
+    if(pumpkinPieStock==foods[4].stockLimit){
+        document.getElementById("pumpkinPieSignD").style.backgroundColor = "rgba(255, 190, 190, 0.808)";
+        pumpkinPieLimit = true;
+    }else if(pumpkinPieStock==foods[4].stockLimit+1){
+        document.getElementById("pumpkinPieSignD").style.backgroundColor = "rgba(255, 190, 190, 0.808)";
+        pumpkinPieLimit = true;
+        pumpkinPieStock = pumpkinPieStock - 1;
+        document.getElementById("pumpkinPieStock").innerHTML = "x" + pumpkinPieStock;
+    }else{
+        document.getElementById("pumpkinPieSignD").style.backgroundColor = "rgba(215, 245, 250, 0.596)";
+        pumpkinPieLimit = false;
+    }
     
 }
 
@@ -111,11 +216,31 @@ function ask(){
     }
 
     if(foods[1].offer!=0){
-        appleCustomer = Math.round(Math.random() * (10 - 1) + 1);
+        appleCustomer = Math.round(Math.random() * (13 - 1) + 1);
     }else if(foods[1].increase!=0){
         appleCustomer = Math.round(Math.random() * (4 - 0) + 0);
     }else{
-        appleCustomer = Math.round(Math.random() * (5 - 0) + 0);
+        appleCustomer = Math.round(Math.random() * (6 - 0) + 0);
+    }
+
+    if(foods[2].lock==true){
+        carrotCustomer = 0;
+    }else if(foods[2].offer!=0){
+        carrotCustomer = Math.round(Math.random() * (20 - 1) + 1);
+    }else if(foods[2].increase!=0){
+        carrotCustomer = Math.round(Math.random() * (5 - 0) + 0);
+    }else{
+        carrotCustomer = Math.round(Math.random() * (10 - 0) + 0);
+    }
+
+    if(foods[4].lock==true){
+        pumpkinPieCustomer = 0;
+    }else if(foods[4].offer!=0){
+        pumpkinPieCustomer = Math.round(Math.random() * (9 - 0) + 0);
+    }else if(foods[4].increase!=0){
+        pumpkinPieCustomer = Math.round(Math.random() * (1 - 0) + 0);
+    }else{
+        pumpkinPieCustomer = Math.round(Math.random() * (3 - 0) + 0);
     }
 
     document.getElementById("askCustomer").innerHTML = `
@@ -129,14 +254,22 @@ function ask(){
                 <img src="images/apple.png" alt="apple">
                 <label id="appleAsk">x${appleCustomer}</label>
             </div>
+            <div class="div__item">
+                <img src="images/carrot.png" alt="carrot">
+                <label id="carrotAsk">x${carrotCustomer}</label>
+            </div>
+            <div class="div__item">
+                <img src="images/pumpkin_pie.png" alt="pumpkinPie">
+                <label id="pumpkinPieAsk">x${pumpkinPieCustomer}</label>
+            </div>
         </div>
     `
 }
 
 function readyToSell(){
-    if((appleStock>=appleCustomer) && (breadStock>=breadCustomer)){
-        ready=true;
-    }else{ready=false;}
+    ((appleStock>=appleCustomer) && (breadStock>=breadCustomer)) ? ready=true : ready=false;
+    ((carrotStock>=carrotCustomer) && (ready==true)) ? ready=true : ready=false;
+    ((pumpkinPieStock>=pumpkinPieCustomer) && (ready==true)) ? ready=true : ready=false;
 }
 
 //funtion acctions
@@ -164,6 +297,18 @@ document.getElementById("appleBut").addEventListener('click', () => {
     makeApple();
 });
 
+document.getElementById("carrotBut").addEventListener('click', () => {
+    makeCarrot();
+});
+
+document.getElementById("pumpkinBut").addEventListener('click', () => {
+    makePumpkin();
+});
+
+document.getElementById("pumpkinPieBut").addEventListener('click', () => {
+    makePumpkinPie();
+});
+
 document.getElementById("vender").addEventListener('click', () => {
     sell();
 });
@@ -189,7 +334,11 @@ document.getElementById("breadOf").addEventListener('click', () => {
         document.getElementById("breadIn").style.backgroundColor = "rgba(127, 255, 212, 0.288)";
         ask();
         sound3.play();
-    }else{alert("Requiere compra del elemento");}
+    }else{
+        swal("Necesitas comprar este elemento.", {
+            icon: "warning",
+        });
+    }
 });
 
 document.getElementById("breadIn").addEventListener('click', () => {
@@ -201,7 +350,11 @@ document.getElementById("breadIn").addEventListener('click', () => {
         document.getElementById("breadIn").style.backgroundColor = "rgba(145, 255, 163, 0.966)";
         ask();
         sound3.play();
-    }else{alert("Requiere compra del elemento");}
+    }else{
+        swal("Necesitas comprar este elemento.", {
+            icon: "warning",
+        });
+    }
 });
 
 document.getElementById("appleOf").addEventListener('click', () => {
@@ -213,7 +366,11 @@ document.getElementById("appleOf").addEventListener('click', () => {
         document.getElementById("appleOf").style.backgroundColor = "rgba(145, 255, 163, 0.966)";
         ask();
         sound3.play();
-    }else{alert("Requiere compra del elemento");}
+    }else{
+        swal("Necesitas comprar este elemento.", {
+            icon: "warning",
+        });
+    }
 });
 
 document.getElementById("appleIn").addEventListener('click', () => {
@@ -225,7 +382,11 @@ document.getElementById("appleIn").addEventListener('click', () => {
         document.getElementById("appleIn").style.backgroundColor = "rgba(145, 255, 163, 0.966)";
         ask();
         sound3.play();
-    }else{alert("Requiere compra del elemento");}
+    }else{
+        swal("Necesitas comprar este elemento.", {
+            icon: "warning",
+        });
+    }
 });
 
 //awards
@@ -310,3 +471,41 @@ document.getElementById("aAppleStockIn").addEventListener('click', () => {
     }
 });
 
+
+//locked buttons
+
+document.getElementById("firstLockedBut").addEventListener('click', () => {
+    if((foods[0].sold < 50)||(foods[1].sold < 50)){
+        swal("Necesitas vender 50 manzanas y 50 panes. Has vendido " + foods[0].sold + " panes y "
+        + foods[1].sold + " manzanas.", {
+            icon: "error",
+          });
+    }else{
+        foods[2].lock = false;
+        ask();
+        swal({
+            icon: "success",
+          });
+        document.getElementById("carrotBut").style.display="flex";
+        document.getElementById("firstLockedBut").style.display="none";
+    }
+});
+
+document.getElementById("secondLockedBut").addEventListener('click', () => {
+    if((foods[1].sold < 150)||(foods[2].sold < 75)){
+        swal("Necesitas vender 150 manzanas y 75 zanahorias. Has vendido " + foods[1].sold + " manzanas y "
+        + foods[2].sold + " zanahorias.", {
+            icon: "error",
+          });
+
+    }else{
+        foods[3].lock = false;
+        foods[4].lock = false;
+        ask();
+        swal({
+            icon: "success",
+          });
+        document.getElementById("pumpkinBut").style.display="flex";
+        document.getElementById("secondLockedBut").style.display="none";
+    }
+});
